87be3c14322aa28d7a498837f0f8dea77b5f9ab2
0xzrf <krishh16@yahoo.com>
2026-03-20 13:55:08 +0530

    added fix to exception for both anchor and line anchor

--- src/types.rs
+++ src/types.rs
@@ -14,6 +14,7 @@ pub enum CharacterClasses {
     Anchor(Vec<CharacterClasses>),
     /// Line anchor defines the end of a line. e.g. dog$ will match `I have a dog`.
     LineAnchor(Vec<CharacterClasses>),
+    ImmAnchor(Vec<CharacterClasses>),
     /// Whitespecee
     WhiteSpace,
 }
@@ -23,13 +24,38 @@ pub struct PatternParser(Vec<CharacterClasses>);
 
 impl From<&str> for PatternParser {
     fn from(pattern: &str) -> Self {
-        let mut peek_itterator = pattern.chars().peekable();
-
+        let mut pattern_peek_iterator = pattern.chars().peekable();
+        let mut is_anchor = false;
+        let mut is_line_anchor = false;
         let mut pattern_vec: Vec<CharacterClasses> = vec![];
 
+
+        if pattern.starts_with("^") {
+            let mut peek_anchor_iter = pattern_peek_iterator.clone();
+            let mut anchor_char_vec = vec![];
+            peek_anchor_iter.next(); // skip `^`
+            while let Some(char) = peek_anchor_iter.next_if(|x| x.ne(&'$')) {
+                anchor_char_vec.push(char);
+            }
+
+            let anchor_vec_string = anchor_char_vec.into_iter().collect::<String>();
+
+            let pattern_parser = PatternParser::from(anchor_vec_string.as_str()).0;
+
+            pattern_vec.push(CharacterClasses::Anchor(pattern_parser));
+            is_anchor = true;
+        }
+
         if pattern.ends_with("$") {
+            let mut peek_line_anchor_iter = pattern_peek_iterator.clone();
             let mut line_anchor_char_vec = vec![];
-            while let Some(char) = peek_itterator.next_if(|x| x.ne(&'$')) {
+
+            // skip the first value if it is also an anchor
+            if peek_line_anchor_iter.peek() == Some(&'^') {
+                peek_line_anchor_iter.next();
+            }
+
+            while let Some(char) = peek_line_anchor_iter.next_if(|x| x.ne(&'$')) {
                 line_anchor_char_vec.push(char);
             }
             let line_vec_string = line_anchor_char_vec.into_iter().collect::<String>();
@@ -37,32 +63,55 @@ impl From<&str> for PatternParser {
             let pattern_parser = PatternParser::from(line_vec_string.as_str()).0;
 
             pattern_vec.push(CharacterClasses::LineAnchor(pattern_parser));
+            is_line_anchor = true;
+        }
+
+        // when it's both anchor and line anchor, the string has to just consist of the provided pattern inside the `^` and `$`
+        if is_anchor && is_line_anchor {
+            let pattern_immutable_anchor_peek_iter = pattern_peek_iterator.clone();
+            let mut immutable_anchor_char_vec = vec![];
+
+            for char in pattern_immutable_anchor_peek_iter {
+                if char.eq(&'^') || char.eq(&'$') {
+                    continue;
+                }
+
+                immutable_anchor_char_vec.push(char);
+            }
+
+            let pattern_parser = PatternParser::from(
+                immutable_anchor_char_vec.into_iter().collect::<String>().as_str(),
+            )
+            .0;
+
+            return PatternParser(vec![CharacterClasses::ImmAnchor(pattern_parser)]); // early return just the Immutable anchor, since it lies as an excpeption
+        } else if is_anchor || is_line_anchor {
             return PatternParser(pattern_vec);
         }
 
-        while peek_itterator.peek().is_some() {
+        while pattern_peek_iterator.peek().is_some() {
             let mut skip_next = true;
-            match peek_itterator.peek() {
+            match pattern_peek_iterator.peek() {
                 Some(x) if x == &'\\' => {
-                    peek_itterator.next();
-                    if peek_itterator.peek() == Some(&'d') {
+                    pattern_peek_iterator.next();
+                    if pattern_peek_iterator.peek() == Some(&'d') {
                         pattern_vec.push(CharacterClasses::Digits)
-                    } else if peek_itterator.peek() == Some(&'w') {
+                    } else if pattern_peek_iterator.peek() == Some(&'w') {
                         pattern_vec.push(CharacterClasses::Characters)
                     };
                 },
                 Some(y) if y == &'[' => {
-                    peek_itterator.next();
+                    pattern_peek_iterator.next();
                     let mut is_neg = false;
-                    if peek_itterator.peek() == Some(&'^') {
+                    if pattern_peek_iterator.peek() == Some(&'^') {
                         is_neg = true;
-                        peek_itterator.next(); // skip `^` and jump to the next value, to start putting it in the Vec<char>
+                        pattern_peek_iterator.next(); // skip `^` and jump to the next value, to start putting it in the Vec<char>
                     }
                     // this is a bracket iterrator, so get all the unique values that're there till `]` occurs
                     let mut find_chars_vec: Vec<char> = vec![];
 
                     // stops when `]` is encountered
-                    while let Some(unique_char) = peek_itterator.next_if(|&x| x != ']') {
+                    while let Some(unique_char) = pattern_peek_iterator.next_if(|&x| x != ']') {
                         find_chars_vec.push(unique_char);
                     }
 
@@ -76,10 +125,10 @@ impl From<&str> for PatternParser {
                     pattern_vec.push(CharacterClasses::WhiteSpace);
                 },
                 Some(start_anchor) if start_anchor == &'^' => {
-                    peek_itterator.next(); // skip the `^` character
+                    pattern_peek_iterator.next(); // skip the `^` character
                     let mut start_anchor_char_vec: Vec<char> = vec![];
 
-                    for char in &mut peek_itterator {
+                    for char in &mut pattern_peek_iterator {
                         start_anchor_char_vec.push(char);
                     }
                     let collected_string = start_anchor_char_vec.into_iter().collect::<String>();
@@ -91,7 +140,7 @@ impl From<&str> for PatternParser {
                 Some(_literal) => {
                     let mut literal_char_vec: Vec<char> = vec![];
 
-                    while let Some(char) = peek_itterator
+                    while let Some(char) = pattern_peek_iterator
                         .next_if(|char| char.ne(&'\\') && char.ne(&'[') && char.ne(&' '))
                     {
                         literal_char_vec.push(char);
@@ -108,7 +157,7 @@ impl From<&str> for PatternParser {
                 None => {},
             }
             if skip_next {
-                peek_itterator.next();
+                pattern_peek_iterator.next();
             }
         }
 
@@ -147,10 +196,11 @@ impl PatternParser {
                     },
                     MatchResultType::Anchor(anchor_match_vec) => {
                         // we will have to assume that position_counter == 0
-                        assert_eq!(*position_counter, 0, "invalid position for anchor match");
+                        let mut anchor_input_peekable_iter = input.char_indices().peekable();
 
                         for pattern in &anchor_match_vec {
-                            let Some((position_counter, char)) = input_peekable.peek() else {
+                            let Some((position_counter, char)) = anchor_input_peekable_iter.peek()
+                            else {
                                 break;
                             };
                             let (matched_result, to_skip) = match pattern
@@ -200,19 +250,18 @@ impl PatternParser {
                                 match pattern.match_char_with_pattern(*char) {
                                     MatchResultType::Char(match_result) => (match_result, 1),
                                     MatchResultType::Literal(literal) => {
-                                        // since we're looking in the reverse order, the position_counter we have is at `input.len() - poisition_counter`
-                                        // relative to `input's` initial position
-                                        // so we will need to the input string from `input.len() - literal.len() - position_counter` -> `input.len() - position_counter`
-                                        // and then assert that the literal and the string slice are the same
-                                        let end_ix = (input.len() - 1) - position_counter;
-                                        let start_ix =
-                                            (input.len() - 1) - literal.len() - position_counter;
+                                        let Some(start_ix) =
+                                            position_counter.checked_sub(literal.len() - 1)
+                                        else {
+                                            return false; // in case that the literal is bigger then the position counter, it's a case where the input isn't big enough for the pattern, hence it doesn't match
+                                        };
 
                                         let input_slice = input
                                             .chars()
                                             .skip(start_ix)
-                                            .take(end_ix)
+                                            .take(literal.len())
                                             .collect::<String>();
+
 
                                         (literal.eq(&input_slice), literal.len())
                                     },
@@ -231,6 +280,71 @@ impl PatternParser {
                             }
                         }
 
+                        return true;
+                    },
+                    MatchResultType::ImmAnchor(imm_anchor) => {
+                        // early return if the length of the value isn't == expected
+                        let mut expected_len = 0u64;
+                        for pattern in &imm_anchor {
+                            match pattern {
+                                CharacterClasses::Digits
+                                | CharacterClasses::WhiteSpace
+                                | CharacterClasses::Characters
+                                | CharacterClasses::NegativeMatch(_)
+                                | CharacterClasses::PositiveMatch(_) => expected_len += 1,
+                                CharacterClasses::Literal(literal) => {
+                                    expected_len += literal.len() as u64
+                                },
+                                _ => {
+                                    unreachable!()
+                                },
+                            }
+                        }
+
+                        if input.len() != expected_len as usize {
+                            return false; // an early return is completely valid if the input isn't exactly the size of the expected pattern
+                        }
+
+                        // we will have to assume that position_counter == 0
+                        let mut imm_anchor_input_peekable_iter = input.char_indices().peekable();
+
+                        for pattern in &imm_anchor {
+                            let Some((position_counter, char)) =
+                                imm_anchor_input_peekable_iter.peek()
+                            else {
+                                break;
+                            };
+                            let (matched_result, to_skip) = match pattern
+                                .match_char_with_pattern(*char)
+                            {
+                                MatchResultType::Char(match_result_type) => (match_result_type, 1),
+                                MatchResultType::Literal(literal) => {
+                                    let literal_len = literal.len();
+
+                                    // get input ref from char_position to char_position + literal_len
+                                    // Get a slice of the input string from char_position to char_position + literal_len
+                                    let input_slice: String = input
+                                        .chars()
+                                        .skip(*position_counter)
+                                        .take(literal_len)
+                                        .collect();
+
+                                    (literal.eq(&input_slice), literal_len)
+                                },
+                                _ => {
+                                    unreachable!()
+                                },
+                            };
+                            if !matched_result {
+                                return false;
+                            } else {
+                                for _ in 0..to_skip {
+                                    input_peekable.next();
+                                }
+                            }
+                        }
+
+                        // if the loop ran without anything being false, then return true
                         return true;
                     },
                 };
@@ -262,6 +376,7 @@ pub enum MatchResultType {
     Literal(String),
     Anchor(Vec<CharacterClasses>),
     LineAnchor(Vec<CharacterClasses>),
+    ImmAnchor(Vec<CharacterClasses>),
 }
 
 impl CharacterClasses {
@@ -286,6 +401,9 @@ impl CharacterClasses {
             CharacterClasses::Anchor(start_anchor) => MatchResultType::Anchor(start_anchor.clone()),
             CharacterClasses::LineAnchor(line_anchor) => {
                 MatchResultType::LineAnchor(line_anchor.clone())
+            },
+            CharacterClasses::ImmAnchor(imm_anchor) => {
+                MatchResultType::ImmAnchor(imm_anchor.clone())
             },
         }
     }
@@ -338,11 +456,6 @@ pub mod pattern_parser_tests {
 
     pub fn assert_pattern_matches(pattern: &str, input: &str) -> bool {
         let parsed_pattern = PatternParser::from(pattern);
-
-        if is_debug_mode() {
-            println!("------------------------------------");
-            println!("parsed patter target: {parsed_pattern:#?} for pattern: {pattern}");
-        }
 
         parsed_pattern.match_input_with_pattern(input)
     }
@@ -466,10 +579,28 @@ pub mod pattern_parser_tests {
         ])];
 
         assert_equality_test(pattern_str, expected_pattern);
+
+        let pattern_str = "\\d\\d \\w$";
+        let expected_pattern = vec![CharacterClasses::LineAnchor(vec![
+            CharacterClasses::Digits,
+            CharacterClasses::Digits,
+            CharacterClasses::WhiteSpace,
+            CharacterClasses::Characters,
+        ])];
+
+        assert_equality_test(pattern_str, expected_pattern);
+
+        let pattern_str = "^orange$";
+        let expected_pattern = vec![CharacterClasses::ImmAnchor(vec![CharacterClasses::Literal(
+            "orange".to_string(),
+        )])];
+
+        assert_equality_test(pattern_str, expected_pattern);
     }
 
     #[test]
     pub fn test_correct_patter_matching_for_input() {
+        // BASIC TESTS
         assert!(assert_pattern_matches("\\d", "1"), "Expected the pattern to match");
         assert!(!assert_pattern_matches("\\d", "w"), "Expected the pattern to not match");
         assert!(assert_pattern_matches("\\d apple", "1 apple"), "Expected the pattern to match");
@@ -499,6 +630,7 @@ pub mod pattern_parser_tests {
             "Expected the pattern to not match"
         );
 
+        // ANCHOR TESTS
         assert!(assert_pattern_matches("^log", "log"), "Expected the pattern to match");
         assert!(assert_pattern_matches("^\\d", "1"), "Expected the pattern to match");
         assert!(assert_pattern_matches("^\\d\\d\\d", "100s"), "Expected the pattern to match");
@@ -507,5 +639,13 @@ pub mod pattern_parser_tests {
             "Expected the pattern to not match"
         );
         assert!(!assert_pattern_matches("^log", "slog"), "Expected the pattern to not match");
+
+        // LINE ANCHOR TESTS
+        assert!(assert_pattern_matches("log$", "this pattern will match this coz it has log"));
+        assert!(assert_pattern_matches("\\d dog has \\w\\w\\ws", "my 1 dog has logs"));
+        assert!(assert_pattern_matches("\\w\\w\\ws", "I have 3 cats"));
+        assert!(!assert_pattern_matches("\\w\\w\\w\\w is a joke", "he is a joke"));
+        assert!(assert_pattern_matches("^orange$", "orange"));
+        assert!(!assert_pattern_matches("^respberry$", "respberry_respberry"));
     }
 }
